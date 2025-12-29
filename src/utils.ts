4bc7d7474c003ecefa77337c3fa42b08c7126d5c
Slava <slava.erof07@gmail.com>
2025-12-29 22:32:52 +0800

    feat: файл с мини-утилитами

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+import { Buffer } from 'buffer';
+
+export function base64UrlEncode(buffer: Buffer): string {
+  return buffer
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
+}
